89a3edc827525567720308d8741973cc9c39c459
Johannes Reiter <johannes@reiter.work>
2022-06-10 14:10:28 +0200

    [TASK] add plugin activation

--- bin/install-plugins.sh
+++ bin/install-plugins.sh
@@ -8,3 +8,14 @@ wp plugin install contact-form-cfdb7
 wp plugin install wordpress-seo
 wp plugin install acf-content-analysis-for-yoast-seo
 wp plugin install wp-fastest-cache
+
+wp plugin activate svg-favicon
+wp plugin activate advanced-custom-fields-pro
+wp plugin activate contact-form-7
+wp plugin activate contact-form-cfdb7
+
+# only on production/staging
+# wp plugin install wps-hide-login
+# wp plugin install wordpress-seo
+# wp plugin install acf-content-analysis-for-yoast-seo
+# wp plugin install wp-fastest-cache
