b7e687d1108d75d08e0a42c36430a70828e37b49
Johannes Reiter <johannes@reiter.work>
2022-06-10 14:06:33 +0200

    [TASK] add script to install default plugins

---
+++ bin/install-plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wp plugin install svg-favicon
+wp plugin install wps-hide-login
+wp plugin install "http://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=ZTE1ZWQzNWEyN2IyOGQ3MmUyN2VkNmE5YzA4MTU0ZmQwODE2ZmNmYzQ4YzJmYzhmZjkxOTFh"
+wp plugin install contact-form-7
+wp plugin install contact-form-cfdb7
+wp plugin install wordpress-seo
+wp plugin install acf-content-analysis-for-yoast-seo
+wp plugin install wp-fastest-cache
